71b494741fb1a815e65bb3c8cc81ae53524d589a
IndorewalaA <abdul.indorewala@ufl.edu>
2023-03-09 13:19:28 -0500

    Decode function

--- main.py
+++ main.py
@@ -7,8 +7,18 @@ def encode(userinput):
     for digit in range(len(password)):
         var1 = int(password[digit]) + 3
         result += str(var1)
+    return result
+
+def decode(userinput):
+    result = ""
+    password = userinput
+    for digit in range(len(password)):
+        var1 = int(password[digit]) - 3
+        result += str(var1)
+    return result
 
 def main():
+    enc = ""
     print("Menu\n"
           "-------------\n"
           "1. Encode\n"
@@ -17,10 +27,11 @@ def main():
     user = print("Please enter an option: ")
     if user == "1":
         userinput = print("Please enter your password to encode: ")
-        encode(userinput)
+        enc = encode(userinput)
         print("Your password has been encoded and stored!")
     if user == "2":
-        pass
+        dec = decode(enc)
+        print(f"The encoded password is {enc}, and the original password is {dec}")
 
 
 
